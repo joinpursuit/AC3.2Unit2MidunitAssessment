713e0f998fc196f1cfa1e2244006e7784c2c6d5c
Madushani Lekam Wasam Liyanage <Shashi@Shashis-MacBook.local>
2016-09-23 13:41:28 -0400

    Assessment Submission

---
+++ AC3.2Unit2MidunitAssessment/AC3.2Unit2MidunitAssessment/ViewController.swift
@@ -0,0 +1,149 @@
+//
+//  ViewController.swift
+//  AC3.2Unit2MidunitAssessment
+//
+//  Created by Madushani Lekam Wasam Liyanage on 9/23/16.
+//  Copyright © 2016 Madushani Lekam Wasam Liyanage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+//init(author: author, work: titleName, scriptDictionary: scriptDict)
+    
+    var counter = 0
+    let poloniusMonologue = ["My liege, and madam, to expostulate",
+                             "What majesty should be, what duty is,",
+                             "What day is day, night night, and time is time,",
+                             "Were nothing but to waste night, day, and time;",
+                             
+                             "Therefore, since brevity is the soul of wit,",
+                             "And tediousness the limbs and outward flourishes,",
+                             "I will be brief. Your noble son is mad."]
+    
+    
+    
+    
+    
+  
+    
+    func displayMonologue() -> String{
+        
+        var nextMonologue = ""
+        let monologueCount = poloniusMonologue.count
+        nextMonologue = poloniusMonologue[counter % monologueCount] //from a youtube video I watched last night https://www.youtube.com/watch?v=S57HUfw9mhM
+        counter += 1
+        
+        return nextMonologue
+    }
+    
+    @IBOutlet weak var lineLabel: UILabel!
+    
+    
+    @IBAction func promptButtonTapped(_ sender: UIButton) {
+        lineLabel.text = displayMonologue()
+    }
+    
+    let macbethScriptData = [
+        ["character": "First Witch",
+         "lines":
+            ["Thrice the brinded cat hath mew'd."]],
+        
+        ["character":"Second Witch",
+         "lines":
+            ["Thrice and once the hedge-pig whined."]],
+        
+        ["character":"Third Witch",
+         "lines":
+            ["Harpier cries 'Tis time, 'tis time."]],
+        
+        ["character":"First Witch",
+         "lines":
+            ["Round about the cauldron go;",
+             "In the poison'd entrails throw.",
+             "Toad, that under cold stone",
+             "Days and nights has thirty-one",
+             "Swelter'd venom sleeping got,",
+             "Boil thou first i' the charmed pot."]],
+        
+        ["character":"ALL",
+         "lines":
+            ["Double, double toil and trouble;",
+             "Fire burn, and cauldron bubble."]],
+        
+        ["character":"Second Witch",
+         "lines":
+            ["Fillet of a fenny snake,",
+             "In the cauldron boil and bake;",
+             "Eye of newt and toe of frog,",
+             "Wool of bat and tongue of dog,",
+             "Adder's fork and blind-worm's sting,",
+             "Lizard's leg and owlet's wing,",
+             "For a charm of powerful trouble,",
+             "Like a hell-broth boil and bubble."]],
+        
+        ["character":"ALL",
+         "lines":
+            ["Double, double toil and trouble;",
+             "Fire burn and cauldron bubble."]]
+    ]
+
+    
+    
+    var characterCounter = 0
+    var lineCounter = 0
+    internal var script: [Script]!
+    internal var author: String = ""
+    internal var titleName: String = ""
+    internal var scriptDict: [[String : Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //   Script.init(author: author, work: titleName, scriptDictionary: scriptDict)
+        self.author = "Shakespeare"
+        self.titleName = "Macbeth"
+        self.scriptDict = macbethScriptData
+        
+    }
+    
+    
+    func updateCharacterName() -> String {
+        
+        let elementsCount = scriptDict.count
+        let currentIndex = characterCounter % elementsCount
+        let character = scriptDict[currentIndex].values.first!
+        
+        characterCounter += 1
+        return String(describing: character)
+    }
+    
+    func updateCharacterLine() -> String {
+//        let elementsCount = scriptDict.count
+//        let currentIndex = lineCounter % elementsCount
+        var lines = ""
+//
+//        lines = scriptDict.startIndex
+//        lineCounter += 1
+//        return String(describing: lines)
+        return lines
+        
+    }
+    
+
+    
+    @IBOutlet weak var characterNameLabel: UILabel!
+    
+    @IBOutlet weak var characterLineLabel: UILabel!
+
+    @IBAction func scriptPromptButtonTapped(_ sender: UIButton) {
+        
+        characterNameLabel.text = updateCharacterName()
+       // characterLineLabel.text = updateCharacterLine()
+        
+        
+    }
+
+}
+

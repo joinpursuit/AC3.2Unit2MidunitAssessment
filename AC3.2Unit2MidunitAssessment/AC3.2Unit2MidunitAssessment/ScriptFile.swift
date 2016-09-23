a78fbebd01196f0b019eaa60e0dc1d592c01daa6
Simone Grant <simonegrant@C4Qs-MacBook-Pro-96.local>
2016-09-23 13:44:21 -0400

    First, and in this case, final commit

---
+++ AC3.2Unit2MidunitAssessment/AC3.2Unit2MidunitAssessment/ScriptFile.swift
@@ -0,0 +1,35 @@
+//
+//  ScriptFile.swift
+//  AC3.2Unit2MidunitAssessment
+//
+//  Created by Simone Grant on 9/23/16.
+//  Copyright © 2016 Simone Grant. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+class Script {
+    struct Monologue {
+        let character: String
+        let lines: [String]
+    }
+    
+    let author: String
+    let work: String
+    let monologues: [Monologue]
+    
+    init(author: String, work: String, scriptDictionary: [[String:Any]]) {
+        self.author = author
+        self.work = work
+        
+        var tempMonologues = [Monologue]()
+        for m in scriptDictionary {
+            if let character = m["character"] as? String, let lines = m["lines"] as? [String] {
+                tempMonologues.append(Monologue(character: character, lines: lines))
+            }
+        }
+        self.monologues = tempMonologues
+    }
+}
